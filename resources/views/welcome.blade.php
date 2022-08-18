f4d330503a52fbe4df78684493220438c89c5a97
Antonio Valença <antoniovalencasouzaneto@gmail.com>
2022-08-18 09:06:00 -0300

    primeiro commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <style>
+            @font-face {
+                font-family: "T20";
+                src: url('fonts/t20font.ttf') format('truetype');
+                font-weight: normal;
+            }
+
+            body {
+                font-family: "T20";
+                font-weight: normal;
+            }
+
+        </style>
+
+        <link rel="stylesheet" href="\css\style.css">
+
+        <script src="/js/scripts.js"></script>
+
+    </head>
+    <body class="base">
+
+        <img src="/img/t20logo.png" alt="Logo">
+
+        <h1>Compêndio Monstruoso</h1>
+
+    </body>
+</html>
