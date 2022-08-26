ff7101e695699dbce1eeb5a3a2287d4c290bf6c0
Antonio Valença <antoniovalencasouzaneto@gmail.com>
2022-08-26 11:09:47 -0300

    reformulação da rota de listagem

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MonstroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,46 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/', function () {
-
-    $monstros = array(
-
-    'goblin' => array(
-        'nome' => 'Goblin',
-        'desafio'  => '1/4',
-        'defesa' => '13',
-        'tipo'  => 'Humanoide',
-        'ataque' => '+5',
-        'deslocamento' => '9 metros',
-    ),
-
-    'esqueleto' => array(
-        'nome' => 'Esqueleto',
-        'desafio'  => '1/2',
-        'defesa' => '15',
-        'tipo'  => 'Morto-Vivo',
-        'ataque' => '+7',
-        'deslocamento' => '12 metros',
-    ),
-
-    'gnoll' => array(
-        'nome' => 'Gnoll',
-        'desafio'  => '1',
-        'defesa' => '16',
-        'tipo'  => 'Humanoide',
-        'ataque' => '+9',
-        'deslocamento' => '15 metros',
-    )
-
-);
-
-    return view('monstros.index', ['monstros' => $monstros]);
+Route::get('/', [ MonstroController::class, 'index'], function () {
+    return view('monstros.index');
 });
-
-
 
 Route::get('/form', function () {
     return view('monstros.form');
 });
+
+// Route::get('/test', [ TimeController::class, 'test'])->name('time.test'); // 'name of function'
