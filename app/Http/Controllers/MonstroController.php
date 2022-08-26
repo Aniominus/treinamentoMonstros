a74d32599eb4748eab2991e121025edb1e2fdf20
Antonio Valença <antoniovalencasouzaneto@gmail.com>
2022-08-26 11:09:27 -0300

    criação do método index

--- app/Http/Controllers/MonstroController.php
+++ app/Http/Controllers/MonstroController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Entities\Monstro;
+use App\Models\Monstro;
 
 class MonstroController extends Controller
 {
-    //
+    /**
+     * Global private declarations.
+     */
+    private $monstros;
+
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Monstro $monstros){
+
+        $this->monstros = $monstros;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $monstros = $this->monstros->all();
+        return view('monstros.index', compact('monstros'));
+    }
+
+
+
 }
