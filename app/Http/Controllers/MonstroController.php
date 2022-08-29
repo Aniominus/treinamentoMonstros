ddc3440d976b27ad9fa6b75968afcd7f254eec34
Antonio Valença <antoniovalencasouzaneto@gmail.com>
2022-08-29 10:57:13 -0300

    Store, create e início do show dos controladores

--- app/Http/Controllers/MonstroController.php
+++ app/Http/Controllers/MonstroController.php
@@ -48,14 +48,64 @@ class MonstroController extends Controller
      */
     public function create()
     {
-        $monstros = $this->monstros;
         $tamanhos = $this->tamanhos;
         $tipos = $this->tipos;
         $ataques = $this->ataques;
-        return view('monstros.form', compact('monstros','tamanhos','tipos', 'ataques'));
+        return view('monstros.form', compact('tamanhos','tipos', 'ataques'));
+    }
+
+/**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Solicita que a model crie um novo registro com as informações do request -?-
+        //e atribui o valor de cada atributo do request ao registro de referência
+        $monstros = $this->monstros->create([
+            'nome' => $request->nome,
+            'desafio' => $request->desafio,
+            'stats_id' => $this->stats->create([
+                'forca' => $request->forca,
+                'destreza' => $request->destreza,
+                'constituicao' => $request->constituicao,
+                'inteligencia' => $request->inteligencia,
+                'sabedoria' => $request->sabedoria,
+                'carisma' => $request->carisma,
+                'deslocamento' => $request->deslocamento,
+                'pontosdevida' => $request->pontosdevida,
+                'pontosdemana' => $request->pontosdemana,
+                'defesa' => $request->defesa
+            ])->id,
+            'tamanho_id' => $request->tamanho,
+            'tipo_id' => $request->tipo,
+        ]);
+        $monstros->ataques = $request->ataques;
+        //retorna a view index, onde as informações que a model time extrai do banco são exibidas
+        return redirect()->route('monstros.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $monstro = $this->monstros->find($id);
+
+        $stats = $this->stats;
+        $tamanhos = $this->tamanhos;
+        $tipos = $this->tipos;
+        $ataques  = $this->ataques;
+        return view('form', compact('stats','tamanhos','tipos','ataques','monstro'));
     }
 
     
+
 
 
 }
