e90d6b64f56b7e6a663e572533274f0c67a7b64e
Antonio Valença <antoniovalencasouzaneto@gmail.com>
2022-08-29 11:55:03 -0300

    criação de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,15 @@ use App\Http\Controllers\MonstroController;
 Route::get('/', [ MonstroController::class, 'index'])->name('monstros.index');
 
 Route::get('/create', [ MonstroController::class, 'create'])->name('monstros.create');
+
+Route::post('/create', [ MonstroController::class, 'store'])->name('monstros.store');
+
+// Route::match(array('PUT','PATCH'), '/update/{id}', [ MonstroController::class, 'update'])->name('monstros.update');
+
+Route::put('/update/{id}', [ MonstroController::class, 'update'])->name('monstros.update');
+
+Route::get('/show/{id}', [ MonstroController::class, 'show'])->name('monstros.show');
+
+Route::get('/edit/{id}', [ MonstroController::class, 'edit'])->name('monstros.edit');
+
+Route::delete('/destroy/{id}', [ MonstroController::class, 'destroy'])->name('monstros.destroy');
